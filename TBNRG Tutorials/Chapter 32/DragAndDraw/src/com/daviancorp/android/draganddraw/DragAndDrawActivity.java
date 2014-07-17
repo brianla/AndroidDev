d47707dd03eed45c3edefd0322377737e9750bd2
Brian La <brianla408@gmail.com>
2014-07-17 14:56:31 -0700

    Chapter 32

---
+++ TBNRG Tutorials/Chapter 32/DragAndDraw/src/com/daviancorp/android/draganddraw/DragAndDrawActivity.java
@@ -0,0 +1,12 @@
+package com.daviancorp.android.draganddraw;
+
+import android.support.v4.app.Fragment;
+
+public class DragAndDrawActivity extends SingleFragmentActivity {
+
+	@Override
+	public Fragment createFragment() {
+		return new DragAndDrawFragment();
+	}
+
+}
